e824ea2abd87a5177c3836482ec4bf4d87e86f45
jxywb <1152326759@qq.com>
2025-05-08 19:42:43 +0800

    add vercel.json

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Nest.js API 服务 - 文件上传与静态文件服务';
+    return 'Nest.js API 服务 - 文件上传与静态文件服务 测试修改发布';
   }
 }
